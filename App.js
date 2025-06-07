793e2903044f74cc7865fb68f250e288e362a3f4
crongcrongcrong <kikihottong@gmail.com>
2025-06-07 21:19:17 +0900

    리액트 네이티브 스타일 구성법

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
+
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
@@ -8,20 +8,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>React Native Practice</Text>
-      <Text style={{ fontSize: 50}}>결과: {number}</Text>
-      <View style={{ flexDirection: "row", gap: 10}}>
-        <Button title="증가" onPress={() => setNumber(number + 1)} />
-        <Button title="감소" onPress={() => setNumber(number - 1)} />
-      </View>
-=======
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
->>>>>>> ae7db85bf9750c4b540ee79c7f3cfd7c5342fb44
+      <Text style={styles.text}>React Native Practice</Text>
+      <Text style={{...styles.text, color: "blue", fontStyle: 10}}>React Native Practice</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -34,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 25,
+    color: "red",
+    fontWeight: "bold",
+  }
 });
